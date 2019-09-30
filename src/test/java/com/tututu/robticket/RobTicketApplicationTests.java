2c26e915eba573405665a0ab476ff91b864da8d9
linchuyao <395643743@qq.com>
2019-09-30 16:36:59 +0800

    first commit

---
+++ src/test/java/com/tututu/robticket/RobTicketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tututu.robticket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RobTicketApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
