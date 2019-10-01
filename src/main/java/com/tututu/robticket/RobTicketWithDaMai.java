d4b9e21c030dba9c5951644d9775b13c3f4cf089
linchuyao <395643743@qq.com>
2019-10-01 12:36:51 +0800

    页面轮询

--- src/main/java/com/tututu/robticket/RobTicketWithDaMai.java
+++ src/main/java/com/tututu/robticket/RobTicketWithDaMai.java
@@ -5,8 +5,12 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author: linchuyao
@@ -14,13 +18,24 @@ import java.util.List;
  */
 public class RobTicketWithDaMai {
     public static void main(String[] args) throws InterruptedException {
-
+        System.setProperty("webdriver.chrome.driver","D:\\DevelopTools\\chromedriver_win32\\chromedriver.exe");
         String domainUrl ="https://www.damai.cn";
         //王力宏
 //        String targetUrl = "https://detail.damai.cn/item.htm?spm=a2oeg.search_category.0.0.29c82244ecC2AH&id=601689680619&clicktitle=%E7%8E%8B%E5%8A%9B%E5%AE%8F%E2%80%9C%E9%BE%99%E7%9A%84%E4%BC%A0%E4%BA%BA2060%E2%80%9D%E4%B8%96%E7%95%8C%E5%B7%A1%E6%BC%94%E7%BB%8D%E5%85%B4%E7%AB%99";
         //林宥嘉
         String targetUrl = "https://detail.damai.cn/item.htm?spm=a2oeg.search_category.0.0.24951f41hmluwS&id=603729509189&clicktitle=%E6%9E%97%E5%AE%A5%E5%98%89idol%E4%B8%96%E7%95%8C%E5%B7%A1%E5%9B%9E%E6%BC%94%E5%94%B1%E4%BC%9A%E2%80%94%E6%9D%AD%E5%B7%9E%E7%AB%99";
-        ChromeDriver chromeDriver = new ChromeDriver();
+
+
+        Map<String, Object> contentSettings = new HashMap<String, Object>();
+        contentSettings.put("images", 2);
+
+        Map<String, Object> preferences = new HashMap<String, Object>();
+        preferences.put("profile.default_content_settings", contentSettings);
+
+        DesiredCapabilities caps = DesiredCapabilities.chrome();
+        caps.setCapability("chrome.prefs", preferences);
+
+        ChromeDriver chromeDriver = new ChromeDriver(caps);
 
         //大麦官网地址
         chromeDriver.navigate().to(domainUrl);
@@ -39,10 +54,13 @@ public class RobTicketWithDaMai {
         //抢票页轮训
         System.out.println("==============================================================");
         System.out.println("开始进行日期及票价选择");
-        String btnText = chromeDriver.findElementByClassName("buybtn").getText();
-        System.out.println("目前抢票页面状态:"+btnText);
-        List<WebElement> list = chromeDriver.findElementByClassName("perform__order__select perform__order__select__performs").findElements(By.xpath("//div[2]/div[@class='select_right_list_item']"));
-        System.out.println(list);
+        while (!chromeDriver.getTitle().contains("确认订单")){
+            String btnText = chromeDriver.findElementByClassName("buybtn").getText();
+            System.out.println("目前抢票页面状态:"+btnText);
+            chromeDriver.navigate().to(targetUrl);
+//            List<WebElement> list = chromeDriver.findElementByClassName("perform__order__select perform__order__select__performs").findElements(By.xpath("//div[2]/div[@class='select_right_list_item']"));
+//            System.out.println(list);
+        }
     }
 
     //扫二维码登录
