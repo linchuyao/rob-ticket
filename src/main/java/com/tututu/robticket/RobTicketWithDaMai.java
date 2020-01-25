ef5f138526464754608ff3a67ea5287fabb5b98b
linchuyao <linchuyao@yingyinggroup.com>
2020-01-25 21:00:42 +0800

    大麦抢票多线程

--- src/main/java/com/tututu/robticket/RobTicketWithDaMai.java
+++ src/main/java/com/tututu/robticket/RobTicketWithDaMai.java
@@ -18,36 +18,28 @@ import java.util.concurrent.*;
  */
 public class RobTicketWithDaMai {
 
+    //cookie存储地址
     private static final String COOKIE_PATH = "src/main/resources/cookies.pkl";
 
+    //chromDriver驱动地址-mac系统
     private static final String CHROME_DRIVER_PATH_MAC = "/Users/tututu/Downloads/chromedriver";
 
+    //chromDriver驱动地址-windows系统
     private static final String CHROME_DRIVER_PATH_WINDOWS = "D:\\DevelopTools\\chromedriver_win32\\chromedriver.exe";
 
     //大麦首页
     private static final String DOMAIN_URL = "https://www.damai.cn";
 
-    //王力宏宁波
-//    private static final String TARGET_URL = "https://detail.damai.cn/item.htm?id=604300702129";
-
-    //王力宏绍兴
-//    private static final String TARGET_URL = "https://detail.damai.cn/item.htm?spm=a2oeg.search_category.0.0.29c82244zUwUg2&id=601689680619&clicktitle=%E7%8E%8B%E5%8A%9B%E5%AE%8F%E2%80%9C%E9%BE%99%E7%9A%84%E4%BC%A0%E4%BA%BA2060%E2%80%9D%E4%B8%96%E7%95%8C%E5%B7%A1%E6%BC%94%E7%BB%8D%E5%85%B4%E7%AB%99";
-
-    //林宥嘉杭州
-//    private static final String TARGET_URL = "https://detail.damai.cn/item.htm?spm=a2oeg.search_category.0.0.20b21f416zuRVD&id=603729509189&clicktitle=%E6%9E%97%E5%AE%A5%E5%98%89idol%E4%B8%96%E7%95%8C%E5%B7%A1%E5%9B%9E%E6%BC%94%E5%94%B1%E4%BC%9A%E2%80%94%E6%9D%AD%E5%B7%9E%E7%AB%99";
-
-    //周杰伦杭州
-//    private static final String TARGET_URL = "https://detail.damai.cn/item.htm?spm=a2oeg.search_category.0.0.5fa628df4gOnID&id=601263739214&clicktitle=%E2%80%9C%E6%B5%A6%E5%8F%91%E8%BF%90%E9%80%9A%E9%AD%94J%E4%BF%A1%E7%94%A8%E5%8D%A1%E2%80%9D%E7%B2%BE%E5%BD%A9%E5%91%88%E7%8C%AE%20%E5%91%A8%E6%9D%B0%E4%BC%A6%E5%98%89%E5%B9%B4%E5%8D%8E%E4%B8%96%E7%95%8C%E5%B7%A1%E5%9B%9E%E6%BC%94%E5%94%B1%E4%BC%9A%20%E6%9D%AD%E5%B7%9E%E7%AB%99";
-
     //蔡依林
-    private static final String TARGET_URL = "https://detail.damai.cn/item.htm?spm=a2oeg.search_category.0.0.1cbc4d159Zsweb&id=611111001414&clicktitle=%E8%94%A1%E4%BE%9D%E6%9E%97%20Ugly%20Beauty%202020%20%E4%B8%96%E7%95%8C%E5%B7%A1%E5%9B%9E%E6%BC%94%E5%94%B1%E4%BC%9A%E4%BD%9B%E5%B1%B1%E7%AB%99";
+    private static final String TARGET_URL = "https://detail.damai.cn/item.htm?id=611259138626";
 
-    //场次
-    private static final int[] SESSIONSET = {1};
+    //选择场次
+    private static final int[] SESSION_SET = {2};
 
     //票位
-    private static final int[] TICKETGRADENSET = {1};
+    private static final int[] TICKET_GRADEN_SET = {3};
 
+    //是否加载照片(不加载可提高网速)
     private static final boolean UNLOAD_IMAGE = false;
 
     private static final ExecutorService ROB_TICKET_THREAD_POOL = new ThreadPoolExecutor(10,20,5, TimeUnit.MINUTES, new LinkedBlockingDeque<>(), new ThreadFactoryBuilder().build(),new ThreadPoolExecutor.AbortPolicy());
@@ -76,6 +68,7 @@ public class RobTicketWithDaMai {
      * @throws ClassNotFoundException
      */
     private static void robTicket() throws InterruptedException, IOException, ClassNotFoundException {
+
         setChromeDriverPath();
 
 
@@ -108,7 +101,7 @@ public class RobTicketWithDaMai {
             System.out.println("====================目前抢票页面状态: " + btnText + "================================");
 
             //选择场次和价位
-//            checkSessionAndTicketGraden(chromeDriver);
+            checkSessionAndTicketGraden(chromeDriver);
 
             //判断页面状态
             if ("立即预订".equals(btnText) || "立即购买".equals(btnText)) {
@@ -127,16 +120,11 @@ public class RobTicketWithDaMai {
 
         Thread.sleep(200);
 
-//        chromeDriver.findElementByLinkText("继续访问").click();
-//        chromeDriver.findElementByLinkText("返回到商品详情").click();
         //选择购票人
         chooseBuyer(chromeDriver);
 
         //提交订单
 //        orderSubmit(chromeDriver);
-//        while (true) {
-//            ;
-//        }
     }
 
 
@@ -242,8 +230,7 @@ public class RobTicketWithDaMai {
      * @param chromeDriver*/
     private static void checkSessionAndTicketGraden(ChromeDriver chromeDriver) {
 
-        System.out.println("==============================================================");
-        System.out.println("开始进行日期及票价选择");
+        System.out.println("==========================开始进行日期及票价选择====================================");
 
         //进入选场次和选票环节
         List<WebElement> performOrderSelects = chromeDriver
@@ -258,8 +245,8 @@ public class RobTicketWithDaMai {
             .className("select_right_list_item"));
 
         //选择场次
-        for (int i = 0; i < RobTicketWithDaMai.SESSIONSET.length; i++) {
-            WebElement webElement = sessionWebElements.get(RobTicketWithDaMai.SESSIONSET[i] - 1);
+        for (int i = 0; i < RobTicketWithDaMai.SESSION_SET.length; i++) {
+            WebElement webElement = sessionWebElements.get(RobTicketWithDaMai.SESSION_SET[i] - 1);
             try {
                 WebElement presellWebElement = webElement.findElement(By.className("presell"));
                 if ("无票".equals(presellWebElement.getText())) {
@@ -274,34 +261,34 @@ public class RobTicketWithDaMai {
             }
         }
 
-//        try {
-//            Thread.sleep(200);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-//        performOrderSelects = chromeDriver
-//                .findElementsByClassName("perform__order__select");
+        performOrderSelects = chromeDriver
+                .findElementsByClassName("perform__order__select");
 
-        //获取票价
-//        WebElement ticketGradeWebElement = performOrderSelects.stream()
-//                .filter(t -> "票档".equals(t.findElement(By.className("select_left")).getText()))
-//                .findFirst().get();
-//
-//
-//        List<WebElement> ticketGradeWebElements = ticketGradeWebElement.findElements(By
-//                .className("select_right_list_item"));
-//
-//
-//        //选择票价
-//        for (int i = 0; i < TICKETGRADENSET.length; i++) {
-//            WebElement webElement = ticketGradeWebElements.get(TICKETGRADENSET[i] - 1);
-//            try {
-//                webElement.findElement(By.className("notticket"));
-//            } catch (Exception e) {
-//                webElement.click();
-//            }
-//        }
+//        获取票价
+        WebElement ticketGradeWebElement = performOrderSelects.stream()
+                .filter(t -> "票档".equals(t.findElement(By.className("select_left")).getText()))
+                .findFirst().get();
+
+
+        List<WebElement> ticketGradeWebElements = ticketGradeWebElement.findElements(By
+                .className("select_right_list_item"));
+
+
+        //选择票价
+        for (int i = 0; i < TICKET_GRADEN_SET.length; i++) {
+            WebElement webElement = ticketGradeWebElements.get(TICKET_GRADEN_SET[i] - 1);
+            try {
+                webElement.findElement(By.className("notticket"));
+            } catch (Exception e) {
+                webElement.click();
+            }
+        }
     }
 
     /**
